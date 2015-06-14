6703fdd719725c185215c888bf8b1151673dba6f
juliezhoucmu <mizhou@andrew.cmu.edu>
2015-06-14 00:43:38 -0400

    得到了access times，并写入文件

--- extra_feature.py
+++ extra_feature.py
@@ -27,27 +27,32 @@ result = pd.DataFrame(pd.read_csv('./train/truth_train.csv',header = 0))
 
 
 
-# merge enrollment and result to 'matrix'
+# merge enrollment and result to 'matrix', get enrollment_id and their result
 matrix = pd.merge(enrollmentIds,result,how='inner',on='enrollment_id')
 del matrix['course_id']
 
+#get access log of this course
+access_path = './train/course/' + course_id + ".csv"
+access = pd.DataFrame(pd.read_csv(access_path,header = 0))
+access_grouped= access.groupby('enrollment_id')
+# access_grouped['time'].transform(lambda x : x.min())
+# get access count for every enrollment id
+access_count = access_grouped.count()
+del access_count['time']
+del access_count['event']
+del access_count['source']
+
+access_count = access_count.reset_index()
 
 
-#extra video visit times
-video_visit_times = []
-for idx, row in matrix.iterrows():
-    video_visit_times.append(full_tree.get_video_visite_times(row.enrollment_id))
-matrix['video_visit_times'] = video_visit_times
+
+matrix = pd.merge(matrix,access_count,on="enrollment_id",how='inner')
+matrix['access_times'] = matrix.object
+del matrix['object']
 
 
-#extra problems visit times
-problems_visit_times = []
-for idx, row in matrix.iterrows():
-    problems_visit_times.append(full_tree.get_problems_visite_times(row.enrollment_id))
-matrix['problems_visit_times'] = problems_visit_times
 
-path = './train/features/' + course_id + '.csv'
-matrix.to_csv(path,index = False,index_label= False,header = 1)
+
 
 
 
